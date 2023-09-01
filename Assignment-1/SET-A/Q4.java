5c520524701bae34daeceee1f4befc2abc76c7de
Saiprasad Tanpure <138004794+TanpureSaiprasad@users.noreply.github.com>
2023-09-01 21:51:23 +0530

    Add files via upload

--- Assignment-1/SET-A/Q4.java
+++ Assignment-1/SET-A/Q4.java
@@ -2,18 +2,14 @@
 arguments) 
 Ans=>*/
 
-import java.util.*;
 class Even
 {
     public static void main(String arg[])
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter limit:");
-        int n=sc.nextInt();
+        int n=arg.length;
         int a[]=new int[n];
-        System.out.println("Enter "+n+" numbers:");
         for(int i=0;i<n;i++)
-            a[i]=sc.nextInt();
+            a[i]=Integer.parseInt(arg[i]);
         System.out.println("all even numbers from array:");
         for(int i=0;i<n;i++)
           if(a[i]%2==0)
